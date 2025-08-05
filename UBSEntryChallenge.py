517635a4e5559ba55534a4a6003f0534a2dc24f3
Ando14-Rookie <adrielorlando.advan@gmail.com>
2025-08-05 10:51:15 +0700

    Final working version

--- UBSEntryChallenge.py
+++ UBSEntryChallenge.py
@@ -5,8 +5,8 @@ import re
 # validStrings= ["abc", "def"]
 # invalidStrings= ["123", "456"]
 #Scroll Test 2
-validStrings= ["aaa", "abb", "acc"]
-invalidStrings= ["bbb", "bcc", "bca"]
+# validStrings= ["aaa", "abb", "acc"]
+# invalidStrings= ["bbb", "bcc", "bca"]
 #Scroll Test 3
 # validStrings= ["abc1", "bbb1", "ccc1"]
 # invalidStrings= ["abc", "bbb", "ccc"]
@@ -20,7 +20,35 @@ invalidStrings= ["bbb", "bcc", "bca"]
 # validStrings= ["foo@abc.com", "bar@def.net"]
 # invalidStrings= ["baz@abc", "qux.com"]
 #Custom Scroll
+validStrings= ["hello again3", "see you soon1", "take care3"]
+invalidStrings= ["1hello", "2again", "3soon"]
 
+currentString = 0
+
+#Used when current and before alphanumeric value are different   
+def makePatternTwo(myContainer:str):
+    #Return String
+    patternCreated = ""
+    #Make Logic so that each container refer to special character
+    if(myContainer.isalpha()):
+        #If container contains only 1 String
+        if len(myContainer) == 1:
+            patternCreated += "[%s]" %str(myContainer)
+            
+        #If container contain more than 1 String
+        else: 
+            patternCreated += "\D+"
+    elif(myContainer.isdigit()):
+        #If container contain only 1 number
+        if len(myContainer) == 1:
+            patternCreated += "[" + myContainer + "]"
+        #If container contain more than 1 number
+        else:
+            patternCreated += "\d+"
+
+    return patternCreated
+
+#Used when current and before character type are different
 def makePattern(myContainer:str):
     #Return String
     patternCreated = ""
@@ -36,7 +64,7 @@ def makePattern(myContainer:str):
     elif(myContainer.isdigit()):
         #If container contain only 1 number
         if len(myContainer) == 1:
-            patternCreated += "[" + myContainer + "]"
+            patternCreated += "\d"
         #If container contain more than 1 number
         else:
             patternCreated += "\d+"
@@ -105,6 +133,7 @@ def checkValidity(patternTest: str):
         #Check whether pattern is correct 
         validStringState = matchPattern(myPattern= patternTest, 
                                         currentString= validStrings[index])
+        
         if not validStringState:
             # This means that if validStringState is "False" then it breaks the loop so that the variable
             # won't be rewritten as true later on
@@ -116,7 +145,9 @@ def checkValidity(patternTest: str):
     for index in range(len(invalidStrings)):
         invalidStringState = (matchPattern(myPattern= patternTest,
                                           currentString= invalidStrings[index]))
+        #For debugging
         print("Current invalidStringState at index %s " %str(index) + "is " + str(invalidStringState))
+        
         if invalidStringState:
             # This means that if invalidStringState is "True" then it breaks the loop so that the variable
             # won't be rewritten as false later on
@@ -131,7 +162,7 @@ def checkValidity(patternTest: str):
     if(validStringState and not (invalidStringState)):
         return True
     else:
-        
+        print("Do recursion to get new pattern")
         return False
 
 #Generate Gree Expression
@@ -146,8 +177,11 @@ def generate_gree_expression(valid_strings, invalid_strings):
     # Analyze character positions
     # Since length is different, but logic same for each String
     # We can find Regex Expression from the first string
-    myString = valid_strings[0]
+    #Reference string used to make the pattern
+    global currentString 
+    myString = valid_strings[currentString]
     length = len(myString)
+
     
     for index in range(length):
 
@@ -155,6 +189,8 @@ def generate_gree_expression(valid_strings, invalid_strings):
         if(index == 0):
             container += myString[index]
             print("1st type: My container is " + container + " at index: " + str(index))
+            #For debugging
+            print("Current return pattern is %s" %str(pattern))
 
         #Do function and clear container if item type 
         #current don't match the previous one
@@ -166,6 +202,8 @@ def generate_gree_expression(valid_strings, invalid_strings):
             #Make new character everytime it doesn't match
             print("Before 2nd type: My container is " + container + " at index: " + str(index))
             pattern += makePattern(myContainer= container)
+            #For debugging
+            print("Current return pattern is %s" %str(pattern))
             #Clear container once function has been made & container cleared
             container = ""
             #Add the new different character to container
@@ -177,15 +215,29 @@ def generate_gree_expression(valid_strings, invalid_strings):
              (str(myString[index]).isalpha() == str(myString[index-1]).isalpha()
               and str(myString[index]).isdigit() == str(myString[index-1]).isdigit()
               )):
-            #If item type 
-            container += myString[index]
+            #If current alphanumeric and before is same, implement differently
+            #Only occur in the beginning
+            if(index < 2 and (str(myString[index]) == str(myString[index-1]))):
+                pattern += makePatternTwo(myContainer= container)
+                #Add the current alphanumeric after makePattern has been conducted
+                #on the previous container
+                container += myString[index]
+                
+            #If content before and current is different
+            else:
+                container += myString[index]
+                
             print("3rd type: My container is " + container + " at index: " + str(index))
+            #For debugging
+            print("Current return pattern is %s" %str(pattern))
         
         #If item type is not alphabetics or digit or a blank space
         else:
              #Make new character everytime doesn't match
             print("Before 4th type: My container is " + container + " at index: " + str(index))
             pattern += makePattern(myContainer= container)
+            #For debugging
+            print("Current return pattern is %s" %str(pattern))
             #Clear container once function has been made & container cleared
             container = ""
             #Add the new not same character to container
@@ -197,21 +249,30 @@ def generate_gree_expression(valid_strings, invalid_strings):
         print("Container not made yet is " + container)
         inPattern = makePattern(myContainer= container)
         pattern += inPattern
+        #For debugging
+        print("Current return pattern is %s" %str(pattern))
 
     #Clear container once pattern 
     container = ""
     #Lastly add $ in the end
     pattern += "$"
-
+    
     #Check pattern with valid strings and invalid strings
     if(checkValidity(patternTest= pattern)):
-        return pattern  
+        #Clean pattern if there is a lot of \D+
+        # print("Before clean pattern: " + pattern)
+        return pattern
     else:
         pattern = "This pattern " +  str(pattern) + " is completely wrong"
-        return pattern
+        print(pattern)
+        #Perhaps we can use recursion everytime the pattern is wrong
+        #We need to make new pattern again by changing the reference validString
+        # Do recursion
+        currentString += 1
+        print("CurrentString is " + str(currentString))
+        if(currentString < len(valid_strings)):
+            generate_gree_expression(validStrings, invalidStrings)
 
 #Print Gree Expression
 result = generate_gree_expression(validStrings, invalidStrings)
-print(result)
-# items = "1333"
-# print(items.__add__("444"))
+print("Final Gree Expression: " + str(result))
